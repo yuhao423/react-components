9cd990dcd4c20aa29b5786a7518b35b261189a88
yuhao <1772156189@qq.com>
2024-07-25 11:33:35 +0800

    feat: 闭包完善

--- src/react-closure/index.tsx
+++ src/react-closure/index.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from 'react'
+import {useCallback, useEffect, useRef, useState} from 'react'
 import { useTimer } from './useTimer'
 
 //react 闭包原因及处理办法
@@ -12,14 +12,12 @@ export default function Closure(){
                 setCount(count + 1)
         }
 
-        useEffect(()=>{
-                setTimeout(()=>{
-                        setCount((count)=>count +1)
-                },1000)
-                console.log('sssbb');
-                
-        },[])
-        // useTimer(update,1000)
+        const clearFn =  useTimer(update,1000)
 
-        return <div>{count}</div>
+        return (
+                <>
+                <div>{count}</div>
+                <button onClick={()=>clearFn()}>停止计时</button>
+                </>
+        )
 }   
--- src/react-closure/useTimer.tsx
+++ src/react-closure/useTimer.tsx
@@ -1,16 +1,35 @@
-import { useEffect, useLayoutEffect, useRef } from "react"
+import { useCallback, useEffect, useLayoutEffect, useRef } from "react"
 
 
 //定时器的封装
+/**
+ * 
+ * @param fn 
+ * @param time
+ * 
+ * 核心原理就是每一次改变状态的时候，将最新的函数赋值给ref，再执行来更改 ！！！
+ */
 export const useTimer = (fn:Function,time?:number)=>{
 
 
     const fnRef = useRef(fn)
 
+    //停止的函数
+    const clearFnRef = useRef<Function>()
+
     //组件挂载之前的时候,useLayoutEffect
     useLayoutEffect(()=>{
+        console.log('我执行了');
+        
+        //将最新的更改状态的函数赋值给ref，这里切记不能加依赖项啊
         fnRef.current = fn;
     })
+
+    //再加一个停止函数，暴露给外面，为什么要用 uesCallback ？
+    const clearFn = useCallback(()=>{
+        clearFnRef.current?.()
+    },[])
+
 
     useEffect(()=>{
 
@@ -18,8 +37,15 @@ export const useTimer = (fn:Function,time?:number)=>{
             fnRef.current()
         },time || 0)
 
-        return ()=>{
+        clearFnRef.current = ()=>{
             clearInterval(timer)
         }
+
+        return ()=>{
+            clearFn()
+        }
     },[])
+
+
+    return clearFn
 }
